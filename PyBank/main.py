d1d7bf797c04985201b542c884e04a56bc3810a6
Daisy Zhang <daisyzhang@Daisys-MacBook-Pro.local>
2021-10-19 22:24:16 -0700

    Updated Pybank/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,17 @@
 import os
 import csv
 
-csv_path = os.path.join('..','resources','budget_data.csv')
+def print_percentages(budget_data):
+    total = int(budget_data[1])
+    print(f"Total: {str(total)}")
+
+
+
+csv_path = os.path.join('resources','budget_data.csv')
 
 with open(csv_path,'r') as budget_csv:
     budget_reader = csv.reader(budget_csv, delimiter = ',')
-    print(budget_reader)
+    header = next(budget_reader)
+
+    for row in budget_reader:
+        print_percentages(row)
