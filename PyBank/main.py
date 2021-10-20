642c9fe8f9ce881b65067f8a9dfb95f81e86ddcf
Daisy Zhang <daisyzhang@Daisys-MacBook-Pro.local>
2021-10-19 22:00:53 -0700

    Updated Pybank/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,8 @@
 import os
 import csv
+
+csv_path = os.path.join("..","resources","budget_data.csv")
+
+with open(csv_path,'r') as budget_csv:
+    budget_reader = csv.reader(budget_csv, delimiter = ',')
+    print(budget_reader)
