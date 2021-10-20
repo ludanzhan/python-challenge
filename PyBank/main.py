6a18ce5f9765965a6c31e4bb40ca54764ab23e51
Daisy Zhang <daisyzhang@Daisys-MacBook-Pro.local>
2021-10-19 23:11:35 -0700

    Updated Pybank/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,38 @@
 import os
 import csv
 
-def print_percentages(budget_data):
-    total = int(budget_data[1])
-    print(f"Total: {str(total)}")
-
-
-
 csv_path = os.path.join('resources','budget_data.csv')
 
 with open(csv_path,'r') as budget_csv:
+
     budget_reader = csv.reader(budget_csv, delimiter = ',')
     header = next(budget_reader)
 
+    total = 0
+    count = 0
+    greatest_increase = 0
+    greatest_decrease = 0
+
     for row in budget_reader:
-        print_percentages(row)
+        num = int(row[1])
+        
+        if num > 0:
+            if num > greatest_increase:
+                greatest_increase = num
+        elif num<0:
+            if num < greatest_decrease:
+                greatest_decrease = num
+
+        total += int(row[1])
+        count +=1
+
+
+    print("Finiancial Analysis\n" 
+          + "------------------------------\n"
+          + "Total Month: " + str(count) + "\n" 
+          + "Total: $"+ str(total) + "\n"
+          + "Greaatest Increase: " + "($" + str(greatest_increase) +")"+ "\n"
+          + "Greatest Decrease: " + "($" + str(greatest_decrease)+ ")" )
+
+        
+       
