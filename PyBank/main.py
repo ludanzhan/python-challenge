7a73b9ebd1c8c1234babb85b6ddc22ee23b1a0d7
Daisy Zhang <daisyzhang@Daisys-MacBook-Pro.local>
2021-10-19 23:27:26 -0700

    Updated Pybank/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,6 +12,9 @@ with open(csv_path,'r') as budget_csv:
     count = 0
     greatest_increase = 0
     greatest_decrease = 0
+    profit = 0
+    loss = 0
+    change = 0
 
     for row in budget_reader:
         num = int(row[1])
@@ -19,26 +22,30 @@ with open(csv_path,'r') as budget_csv:
         if num > 0:
             if num > greatest_increase:
                 greatest_increase = num
+                profit += num
         elif num<0:
             if num < greatest_decrease:
                 greatest_decrease = num
+                loss += num
 
         if int(row[1]) == greatest_increase:
             increase = str(row[0])
         elif int(row[1]) == greatest_decrease:
             decrease = str(row[0])
-
-
+        
         total += int(row[1])
         count +=1
+        change = (profit + loss)/count
 
 
     print("Finiancial Analysis\n" 
           + "------------------------------\n"
           + "Total Month: " + str(count) + "\n" 
           + "Total: $"+ str(total) + "\n"
+          + "Average Change: $" + str(change) + "\n"
           + "Greaatest Increase: " + increase+ " ($" + str(greatest_increase) +")"+ "\n"
           + "Greatest Decrease: " +decrease+ " ($" + str(greatest_decrease)+ ")" )
+      
 
         
        
